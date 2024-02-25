845e7a898540285d6a9e054c4bd77eb187889916
Diego Barreto da Cunha <github@diegobarreto.dev>
2024-02-24 22:59:01 -0300

    .

--- tailwind-attr-variant.js
+++ tailwind-attr-variant.js
@@ -1,71 +1,70 @@
+const pseudoClassesModifiers = [
+  "hover",
+  "focus",
+  "focus-within",
+  "focus-visible",
+  "active",
+  "visited",
+  "target",
+  "first",
+  "last",
+  "only",
+  "odd",
+  "even",
+  "first-of-type",
+  "last-of-type",
+  "only-of-type",
+  "empty",
+  "disabled",
+  "enabled",
+  "checked",
+  "indeterminate",
+  "default",
+  "required",
+  "valid",
+  "invalid",
+  "in-range",
+  "out-of-range",
+  "placeholder-shown",
+  "autofill",
+  "read-only",
+  "before",
+  "after",
+  "first-letter",
+  "first-line",
+  "marker",
+  "selection",
+  "file",
+  "backdrop",
+  "rtl",
+  "ltr",
+  "open",
+  "placeholder",
+];
+
+const mediaModifiers = [
+  "sm",
+  "md",
+  "lg",
+  "xl",
+  "2xl",
+  "min-[",
+  "max-[",
+  "max-sm",
+  "max-md",
+  "max-lg",
+  "max-xl",
+  "max-2xl",
+  "dark",
+  "portrait",
+  "landscape",
+  "motion-safe",
+  "motion-reduce",
+  "contrast-more",
+  "contrast-less",
+  "print",
+];
 module.exports = function ({ types: t }) {
-  const pseudoClassesModifiers = [
-    "hover",
-    "focus",
-    "focus-within",
-    "focus-visible",
-    "active",
-    "visited",
-    "target",
-    "first",
-    "last",
-    "only",
-    "odd",
-    "even",
-    "first-of-type",
-    "last-of-type",
-    "only-of-type",
-    "empty",
-    "disabled",
-    "enabled",
-    "checked",
-    "indeterminate",
-    "default",
-    "required",
-    "valid",
-    "invalid",
-    "in-range",
-    "out-of-range",
-    "placeholder-shown",
-    "autofill",
-    "read-only",
-    "before",
-    "after",
-    "first-letter",
-    "first-line",
-    "marker",
-    "selection",
-    "file",
-    "backdrop",
-    "rtl",
-    "ltr",
-    "open",
-    "placeholder",
-  ];
-
-  const mediaModifiers = [
-    "sm",
-    "md",
-    "lg",
-    "xl",
-    "2xl",
-    "min-[",
-    "max-[",
-    "max-sm",
-    "max-md",
-    "max-lg",
-    "max-xl",
-    "max-2xl",
-    "dark",
-    "portrait",
-    "landscape",
-    "motion-safe",
-    "motion-reduce",
-    "contrast-more",
-    "contrast-less",
-    "print"
-  ];
-
   return {
     visitor: {
       JSXOpeningElement(path) {
@@ -76,19 +75,29 @@ module.exports = function ({ types: t }) {
           if (t.isJSXAttribute(attribute) && attribute.name) {
             const attributeName = attribute.name.name;
 
-            if (
-                pseudoClassesModifiers.includes(attributeName) ||
-                mediaModifiers.includes(attributeName)
-            ) {
-              const value =
-                attribute.value.value || attribute.value.expression.value;
-              const tailwindClass = `${attributeName}:${value}`;
-              newClasses.push(tailwindClass);
-              attributesToRemove.push(attribute);
+            // Combina as duas listas para simplificar a lógica
+            const allModifiers = [...pseudoClassesModifiers, ...mediaModifiers];
+
+            if (allModifiers.includes(attributeName)) {
+              // Assegura que o valor está definido, seja diretamente ou através de expressão
+              let value = attribute.value.value;
+              if (!value && attribute.value.expression) {
+                value =
+                  attribute.value.expression.type === "StringLiteral"
+                    ? attribute.value.expression.value
+                    : "";
+              }
+
+              if (value) {
+                const tailwindClass = `${attributeName}:${value}`;
+                newClasses.push(tailwindClass);
+                attributesToRemove.push(attribute);
+              }
             }
           }
         });
 
+        // Modifica ou adiciona o atributo className
         if (newClasses.length > 0) {
           const existingClassAttribute = path.node.attributes.find(
             (attr) => attr.name.name === "className"
@@ -105,9 +114,10 @@ module.exports = function ({ types: t }) {
           }
         }
 
-        attributesToRemove.forEach((attr) => {
-          path.node.attributes.splice(path.node.attributes.indexOf(attr), 1);
-        });
+        // Remove os atributos processados
+        attributesToRemove.forEach((attr) =>
+          path.node.attributes.splice(path.node.attributes.indexOf(attr), 1)
+        );
       },
     },
   };
